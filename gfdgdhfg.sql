5eb8afab124564733ae206af80ff043b078b0869
simma-work <479921347@qq.com>
2018-11-07 12:44:59 +0800

    D

---
+++ gfdgdhfg.sql
@@ -0,0 +1,7 @@
+prompt Importing table cstest.cs_base_accessory...
+set feedback off
+set define off
+insert into cstest.cs_base_accessory (ID, ACCESSORY_CODE, ACCESSORY_TYPE, ACCESSORY_NAME, ACCESSORY_MODULE, ACCESSORY_OBJECT_ID, ACCESSORY_SOURCE, IS_ENABLED, COMMENTS, CREATED_BY, CREATED_TIME, LAST_UPDATED_BY, LAST_UPDATED_TIME, STAT)
+values (1313, 'images/cs/201804/UID_3c399e7a-f7a3-4bdc-93ab-990734b76118.jpg', 'jpg', 'UID_3c399e7a-f7a3-4bdc-93ab-990734b76118.jpg', 'PCSO-PC', 'S201804270004', 'PC', 1, 'PC端上传图片', 'SYSTEM', '01-9月 -18 12.11.45.000000 下午', 'SYSTEM', '01-9月 -18 12.11.45.000000 下午', null);
+
+prompt Done.
